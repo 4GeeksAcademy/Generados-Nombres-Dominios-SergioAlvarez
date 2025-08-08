643ad7d2494b900d412d97581f8374a6750f2586
Sergioalv15 <sergioalv961@gmail.com>
2025-08-08 18:43:33 +0000

    [Terminado proyyecto] He agregado el 'hack' del que se habla en la pagina del proyecto y agregado mas terminaciones de paginas.

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domain = ['.com','.es','.io']
+
+
+  
+  for (let i = 0; i < pronoun.length; i++) {
+    
+    
+    for(let j = 0; j < adj.length; j++){
+      
+      
+      for(let k = 0; k < noun.length; k++){
+        
+        
+        for(let x  = 0; x < domain.length; x++){
+
+
+          if((noun[k] == 'racoon') && (domain[x] == '.com')){
+            ((noun[k] = 'ra') && (domain[x] = '.coom'))
+            console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[x]}`);
+            ((noun[k] = 'racoon') && (domain[x] = '.com' ))
+          }
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[x]}`)
+      }
+    }
+  }
+}
