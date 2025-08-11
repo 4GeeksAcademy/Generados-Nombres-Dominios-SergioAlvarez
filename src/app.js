92de86f9227824d0101bae9dfa39c927dbb5812e
Sergioalv15 <sergioalv961@gmail.com>
2025-08-11 18:15:57 +0000

    [proyecto personal terminado]
    Me devuelve numeros aleatorios entre 140 y 180

--- src/app.js
+++ src/app.js
@@ -1,39 +1,37 @@
+console.log("hola")
 
+let boton = document.getElementById("boton")
 
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let domain = ['.com','.es','.io']
-
+boton.addEventListener("click", () => {
+  
+  const valores = () => {
+    let primeraCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let segundaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let segundaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let terceraCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let terceraCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let cuartaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let cuartaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let quintaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let quintaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let sextaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let sextaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let septimaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let septimaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let octavaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let octavaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let novenaCelda = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    let novenaCelda2 = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    
+    return `${primeraCelda} // ${segundaCelda} // ${terceraCelda} || ${cuartaCelda} & ${cuartaCelda2} // ${quintaCelda} & ${quintaCelda2} // ${sextaCelda} & ${sextaCelda2} || ${septimaCelda} & ${septimaCelda2} // ${octavaCelda} & ${octavaCelda2} // ${novenaCelda} & ${novenaCelda2}`
+  }
+    
+    
+    
 
   
-  for (let i = 0; i < pronoun.length; i++) {
-    
-    
-    for(let j = 0; j < adj.length; j++){
-      
-      
-      for(let k = 0; k < noun.length; k++){
-        
-        
-        for(let x  = 0; x < domain.length; x++){
+  document.getElementById("numero").innerHTML = valores()
 
+})
 
-          if((noun[k] == 'racoon') && (domain[x] == '.com')){
-            ((noun[k] = 'ra') && (domain[x] = '.coom'))
-
-
-            console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[x]}`);
-
-
-            ((noun[k] = 'racoon') && (domain[x] = '.com' ))
-
-
-          }
-
-          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[x]}`)
-
-      }
-    }
-  }
-}
+console.log()
